512939a0c95acb94b3da2e262d3dde6b75720b1a
Ansh <anshkateliya2004@gmail.com>
2026-03-17 19:13:48 +0530

    added recommended friend functionality. resolve recomended user fetching error

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 import {
@@ -7,7 +8,14 @@ import {
   sendFriendRequest,
 } from "../lib/api";
 import { Link } from "react-router";
-import { UsersIcon } from "lucide-react";
+import {
+  CheckCircleIcon,
+  MapPinIcon,
+  UserPlusIcon,
+  UsersIcon,
+} from "lucide-react";
+import NoFriendFound from "../components/NoFriendFound";
+import { getLanguageFlag } from "../components/FriendCard";
 
 const HomePage = () => {
   const queryClient = useQueryClient();
@@ -62,13 +70,116 @@ const HomePage = () => {
             <span className="loading loading-spinner loading-lg" />
           </div>
         ) : friends.length === 0 ? (
-          <p>No Friends yet</p>
+          <NoFriendFound />
         ) : (
-          <div></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {friends.map((friend) => (
+              <FriendCard key={friend._id} friend={friend} />
+            ))}
+          </div>
         )}
+
+        <section>
+          <div className="mb-6 sm:mb-8">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+                  Meet New Learners
+                </h2>
+                <p className="opacity-70">
+                  Discover the perfact language exchange partner based on your
+                  profile
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {loadingUsers ? (
+            <div className="flex justify-center py-12">
+              <span className="loading loading-spinner loading-lg" />
+            </div>
+          ) : recommendUser.length === 0 ? (
+            <div className="card bg-base-200 p-6 text-center">
+              <h3 className="font-semibold text-lg mb-2">
+                No recommendations available
+              </h3>
+              <p className="text-base-content opacity-70">
+                Check back later for new language partners!
+              </p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {recommendUser.map((user) => {
+                const hasRequestBeenSent = outRequestIds.has(user.id);
+                return (
+                  <div
+                    key={user._id}
+                    className="card bg-base-200 hover:shadow-lg transition-all duration-300"
+                  >
+                    <div className="card-body p-5 space-y-4">
+                      <div className="flex items-center gap-3">
+                        <div className="avatar size-16 rounded-full">
+                          <img src={user.profilePic} alt={user.fullName} />
+                        </div>
+                        <div>
+                          <h3 className="font-semibold text-lg">
+                            {user.fullName}
+                          </h3>
+                          {user.location && (
+                            <div className="flex items-center text-xs opacity-70 mt-1">
+                              <MapPinIcon className="size-3 mr-1" />
+                              {user.location}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      {/* Language with flags */}
+                      <div className="flex flex-wrap fap-1.5">
+                        <span className="badge badge-secondary">
+                          {getLanguageFlag(user.nativeLanguage)}
+                          Native: {capitialize(user.nativeLanguage)}
+                        </span>
+                        <span className="badge badge-secondary">
+                          {getLanguageFlag(user.learningLanguage)}
+                          Learning: {capitialize(user.learningLanguage)}
+                        </span>
+                      </div>
+
+                      {user.bio && (
+                        <p className="text-sm opacity-70">{user.bio}</p>
+                      )}
+
+                      <button
+                        className={`btn w-full mt-2 ${
+                          hasRequestBeenSent ? "btn-disabled" : "btn-primary"
+                        }`}
+                        onClick={() => sendReqMutation(user._id)}
+                        disabled={hasRequestBeenSent || isPending}
+                      >
+                        {hasRequestBeenSent ? (
+                          <>
+                            <CheckCircleIcon className="size-4 mr-2" />
+                            Request Sent
+                          </>
+                        ) : (
+                          <>
+                            <UserPlusIcon className="size-4 mr-2" />
+                            Send Friend Request
+                          </>
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </section>
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
+export const capitialize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
